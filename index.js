73ca444519201613b6a105066c70b174f98e68dd
downlouder <118766413+downlouder@users.noreply.github.com>
2023-01-08 02:57:34 +0200

    rps-ui 2.0

--- index.js
+++ index.js
@@ -13,24 +13,39 @@ function getComputerChoice() {
 }
 //end
 
+function disableButtons() {
+    buttons.forEach((button) => {
+        button.disabled = true;
+    });
+}
+
 function playRound(playerSelection, computerSelection) {
     // let playerScore = 0;
     // let computerScore = 0;
+    let wonGame = '';
     let result = document.getElementById('result');
     let string = '';
     let player = playerSelection.toLowerCase();
     let computer = computerSelection.toLowerCase();
     console.log(`${player}, ${computer}`)
     if (player === computer) {
-        string = "Tie, nobody won or lost";
+        string = "Tie, nobody won or lost this round";
     }
     else if ((player === "scissors" && computer === "paper") || (player === "paper" && computer === "rock") || (player === "rock" && computer === "scissors")) {
-        string = `You won, ${playerSelection} beats ${computerSelection}`;
+        string = `You won this round, ${playerSelection} beats ${computerSelection}`;
         playerScore++;
+        if (playerScore === 5) {
+            wonGame = "My Congratulations, You won this game!!!";
+            disableButtons();
+        }
     }
     else if ((player === "paper" && computer === "scissors") || (player === "rock" && computer === "paper") || (player === "scissors" && computer === "rock")) {
-        string = `Computer won, ${computerSelection} beats ${playerSelection}`;
+        string = `Computer won this round, ${computerSelection} beats ${playerSelection}`;
         computerScore++;
+        if (computerScore === 5) {
+            wonGame = "I'm sorry, but Computer won this game, I hope you have better luck in the next game";
+            disableButtons();
+        }
     }
-    return result.innerHTML = string + "<br>" + `Player: ${playerScore} | Computer: ${computerScore}`;
+    return result.innerHTML = `<p class="round">${string}</p><p class="info">Player: ${playerScore} | Computer: ${computerScore}</p><p class="game">${wonGame}</p>`;
 }
