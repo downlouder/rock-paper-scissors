d7798cea8c97d91c713f49bc647b552156ca86fa
downlouder <118766413+downlouder@users.noreply.github.com>
2023-01-08 02:40:29 +0200

    rps-ui 1.0

--- index.js
+++ index.js
@@ -1,54 +1,36 @@
-const items = ["Rock", "Paper", "Scissors"]
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice(items);
-const round = playRound(playerSelection, computerSelection);
+let playerScore = 0;
+let computerScore = 0;
+const buttons = document.querySelectorAll('input');
+//basic mechanic start
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playRound(button.id, getComputerChoice()); 
+    });
+});
+function getComputerChoice() {
+    let items = ["Rock", "Paper", "Scissors"];
+    return items[Math.floor(Math.random() * items.length)];
+}
+//end
 
-function getComputerChoice(obj) {
-    let indexOfAnswer = Math.floor(Math.random() * obj.length);
-    return obj[indexOfAnswer];
-}
-function getPlayerChoice() {
-    let answer = prompt("Choice Rock, Paper or Scissors");
-    return answer;
-}
 function playRound(playerSelection, computerSelection) {
+    // let playerScore = 0;
+    // let computerScore = 0;
+    let result = document.getElementById('result');
+    let string = '';
     let player = playerSelection.toLowerCase();
     let computer = computerSelection.toLowerCase();
     console.log(`${player}, ${computer}`)
     if (player === computer) {
-        return "Tie, nobody won or lost";
+        string = "Tie, nobody won or lost";
     }
-    else if (player === "scissors" && computer === "paper") {
-        return "You won, Scissors beats Paper";
+    else if ((player === "scissors" && computer === "paper") || (player === "paper" && computer === "rock") || (player === "rock" && computer === "scissors")) {
+        string = `You won, ${playerSelection} beats ${computerSelection}`;
+        playerScore++;
     }
-    else if (player === "paper" && computer === "scissors") {
-        return "Computer won, Scissors beats Paper";
+    else if ((player === "paper" && computer === "scissors") || (player === "rock" && computer === "paper") || (player === "scissors" && computer === "rock")) {
+        string = `Computer won, ${computerSelection} beats ${playerSelection}`;
+        computerScore++;
     }
-    else if (player === "paper" && computer === "rock") {
-        return "You won, Paper beats Rock";
-    }
-    else if (player === "rock" && computer === "paper") {
-        return "Computer won, Paper beats Rock";
-    }
-    else if (player === "rock" && computer === "scissors") {
-        return "You won, Rock beats Scissors";
-    }
-    else if (player === "scissors" && computer === "rock") {
-        return "Computer won, Rock beats Scissors";
-    }
-    else {
-        return "Please enter correct words";
-    }
-}
-function game(round) {
-    // let computerWins = 0;
-    // let playerWins = 0;
-    // while (computerWins < 5 || playerWins < 5) {
-    //     round;
-    //     if()
-    // }
-    return round;
-}
-for (let i = 0; i < 5; i++) {
-    console.log(game(round));
+    return result.innerHTML = string + "<br>" + `Player: ${playerScore} | Computer: ${computerScore}`;
 }
